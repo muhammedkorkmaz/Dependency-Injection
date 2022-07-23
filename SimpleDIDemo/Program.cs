e2fede42c68029243a8e4949b69d9f2862b6f9dc
muhammedkorkmaz <muhammedkorkmaz10@outlook.com>
2022-07-23 16:09:33 +0300

    Dependency Injection

---
+++ SimpleDIDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SimpleDIDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
